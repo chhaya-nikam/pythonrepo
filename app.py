08ac339251e60ab00e416627460d4f4f9ca5e8aa
chhaya-nikam <chhaya.vikrant.nikam@pwc.com>
2025-08-06 17:36:13 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+def greet_user(name):
+    """
+    Greets the user with the provided name.
+
+    Args:
+        name (str): The name of the user.
+
+    Returns:
+        str: A greeting message.
+    """
+    return f"Hello, {name}!"
